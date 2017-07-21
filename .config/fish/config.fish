d9046789487e397b0f3d57984a3a822e12f6e788
Tom O'Dwyer <24863179+tomodwyer@users.noreply.github.com>
2017-07-21 20:50:29 +0100

    Switch from functions to aliases in fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
+. ~/.config/fish/aliases.fish
+
 function brewme
   brewup
   brewtidy
@@ -12,68 +14,4 @@ end
 function brewup
   brew update
   brew upgrade
-end
-
-function c
-  clear
-end
-
-function cp
-  cp -i
-end
-
-function dud
-  du -d 1 -h
-end
-
-function duf
-  du -sh *
-end
-
-function fd
-  find . -type d -name
-end
-
-function ff
-  find . -type f -name
-end
-
-function h
-  history
-end
-
-function help
-  man
-end
-
-function l
-  ls -lFh
-end
-
-function la
-  ls -lAFh
-end
-
-function ll
-  ls -l
-end
-
-function lr
-  ls -tRFh
-end
-
-function lt
-  ls -ltFh
-end
-
-function mv
-  mv -i
-end
-
-function p
-  ps -f
-end
-
-function rm
-  rm -i
 end
